5370037647f656c07a2de4f336e546f9de8a3dbb
MVReyna <vickyreyna79@gmail.com>
2026-06-15 20:15:05 -0300

    Automatización ID 04(reserva)

--- cypress/e2e/myTestRestfulBooker.cy.js
+++ cypress/e2e/myTestRestfulBooker.cy.js
@@ -16,11 +16,20 @@ describe('Reservas en Shady Meadows B&B', () => {
         cy.get('a[href="/reservation/1?checkin=2026-06-15&checkout=2026-06-16"]').click()
         cy.get('#doReservation').click()
 
-        //   cy.get('[placeholder="Firstname"]').type('Juan')
-        //   cy.get('[placeholder="Lastname"]').type('Perez')
-        //   cy.get('[placeholder="Email"]').type(`mail${Date.now()}@gmail.com`)
-        //   cy.get('[placeholder="Phone"]').type('01164666830')
-        //   cy.get('button.btn-primary.w-100').click()
+    })
+
+    it('Completar el formulario con datos válidos (nombre, apellido, email, teléfono y fechas) ', () => {
+        cy.get('a[href="#booking"]').click()
+
+        cy.get('a[href="/reservation/1?checkin=2026-06-15&checkout=2026-06-16"]').click()
+        cy.get('#doReservation').click()
+
+        cy.get('[placeholder="Firstname"]').type('Juan')
+        cy.get('[placeholder="Lastname"]').type('Perez')
+        cy.get('[placeholder="Email"]').type(`mail${Date.now()}@gmail.com`)
+        cy.get('[placeholder="Phone"]').type('01164666830')
+        cy.get('button.btn-primary.w-100').click()
 
     })
+
 })
